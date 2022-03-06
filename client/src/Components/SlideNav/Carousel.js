0881b43f58ce03431a46037642971121ee39fee2
dylandupasquier <dylandupasquier@gmail.com>
2022-03-06 13:30:14 -0800

    added photos to carousel

--- client/src/Components/SlideNav/Carousel.js
+++ client/src/Components/SlideNav/Carousel.js
@@ -3,10 +3,14 @@ import "react-slideshow-image/dist/styles.css";
 // import { Slide } from "react-slideshow-image";
 import { Fade } from "react-slideshow-image";
 import img1 from "../LocalAssets/geyser-yelowstone-burst_h.png";
-import img2 from "../LocalAssets/hiker-in-north-window-arch-royalty-free-image-1588786762.jpg";
-import img3 from "../LocalAssets/national_parks_sustainability_feature.jpg";
+import img2 from "../LocalAssets/GRp433jw6BKfCyL86nk8jc-970-80.jpg.webp";
+import img3 from "../LocalAssets/hiker-in-north-window-arch-royalty-free-image-1588786762.jpg";
+import img4 from "../LocalAssets/ku5EqMv2ZWTQJvMb3XCmeb-970-80.jpg.webp";
+import img5 from "../LocalAssets/national_parks_sustainability_feature.jpg";
+import img6 from "../LocalAssets/poAYm6mb5ay4TBSYuMAWs4-970-80.jpg.webp";
+import img7 from "../LocalAssets/ukUkjVfuPhJadUKzSS4Kmm-970-80.jpg.webp";
+import img8 from "../LocalAssets/ZiVxAfNzdnvjEigJuKXxJW-970-80.jpg.webp";
 
-// const slideImages = [`${img1}`, `${img2}`, `${img3}`];
 
 const fadeImages = [
   {
@@ -18,6 +22,22 @@ const fadeImages = [
   {
     url: `${img3}`,
   },
+  {
+    url: `${img4}`,
+  },
+  {
+    url: `${img5}`,
+  },
+  {
+    url: `${img6}`,
+  },
+  {
+    url: `${img7}`,
+  },
+  {
+    url: `${img8}`,
+  },
+  
 ];
 
 const slides = fadeImages.map((fadeImage, index) => {
