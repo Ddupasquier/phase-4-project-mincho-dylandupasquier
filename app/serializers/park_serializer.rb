bc0213aead40a339f8e22d8d238681c385bbe9a8
dylandupasquier <dylandupasquier@gmail.com>
2022-03-06 15:10:15 -0800

    stuff

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,3 +1,3 @@
 class ParkSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :description, :latitude, :longitude, :states, :weatherInfo, :images
 end
--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -4,31 +4,29 @@ import "../grid-card-styles.css";
 import ParkCard from "./ParkCard";
 
 function Main() {
-  // const [allParks, setAllParks] = useState([]);
+  const [allParks, setAllParks] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(
-  //     "https://developer.nps.gov/api/v1/parks?limit=10&start=0&q=data&api_key=gNJoHqgkJgXhDR0HejOhKQSVeu9iW825MiQB3NpL"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setAllParks(data);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch("/parks")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllParks(data);
+      });
+  }, []);
 
   // console.log(allParks);
 
   // This will be usable once I have something to render
-  // const eachPark = allParks.map((park) => {
-  //   return <ParkCard />
-  // })
+  const eachPark = allParks.map((park) => {
+    return <ParkCard />;
+  });
 
   return (
-    
     <main>
       <SortSearch />
       <div className="grid-container">
-        {/* {eachPark} */}
+        {eachPark}
+        {/* <div className="grid-square">1</div>
         <div className="grid-square">1</div>
         <div className="grid-square">1</div>
         <div className="grid-square">1</div>
@@ -47,15 +45,13 @@ function Main() {
         <div className="grid-square">1</div>
         <div className="grid-square">1</div>
         <div className="grid-square">1</div>
-        <div className="grid-square">1</div>
-        <div className="grid-square">1</div>
+        <div className="grid-square">1</div> */}
       </div>
       <div className="tocenter">
         <button className="btn">Back</button>{" "}
         <button className="btn">More</button>
       </div>
     </main>
-    
   );
 }
 
